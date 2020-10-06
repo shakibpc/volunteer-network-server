f109bd7ca50744cf3955d47deaebd1cc020134a5
shakibpc <shakibhossain1020@gamil.com>
2020-10-07 00:14:32 +0600

    sdfsd

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ client.connect(err => {
  })
 
 app.get('/registerUserEvents', (req, res) => {
-  // console.log(req.query.email);
+  console.log(req.query.email);
   addEvents.find({email: req.query.email})
   .toArray((err, documents) => {
     res.send(documents);
