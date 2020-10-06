2cd1ddc755794e49719f6e8532f3616ae6dc1709
shakibpc <shakibhossain1020@gamil.com>
2020-10-06 23:29:39 +0600

    add

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express')
-require('dotenv').config()
+// require('dotenv').config()
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { ObjectId } = require('mongodb');
+// const { ObjectId } = require('mongodb');
 const port = 5000
 
 const app = express()
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.giumd.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
