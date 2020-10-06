b624533cb427c7305497e619bb1ccc5f9811e10d
shakibpc <shakibhossain1020@gamil.com>
2020-10-06 22:31:54 +0600

    alomost done

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const port = 5000
+
+const app = express()
+
+app.use(cors());
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://volunteer-network:volunteerNetwork7373@cluster0.j05gt.mongodb.net/volunteer-network?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const addEvents = client.db("volunteer-network").collection("eventRegister");
+
+ console.log('DB Connected');
+
+ app.post('/addNewRegister', (req, res) => {
+  const newRegister = req.body;
+  addEvents.insertOne(newRegister)
+  .then(result =>{
+    res.send(result.insertedCount > 0);
+    console.log(result);
+  })
+  console.log(newRegister);
+ })
+
+app.get('/registerUserEvents', (req, res) => {
+  // console.log(req.query.email);
+  addEvents.find({email: req.query.email})
+  .toArray((err, documents) => {
+    res.send(documents);
+   })
+  })
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello!')
+})
+
+app.listen(port);
