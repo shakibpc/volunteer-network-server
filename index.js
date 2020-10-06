6540702676b5a963757a73ab0812cbc3de9b2909
shakibpc <shakibhossain1020@gamil.com>
2020-10-06 23:16:27 +0600

    add some

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const express = require('express')
+require('dotenv').config()
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { ObjectId } = require('mongodb');
 const port = 5000
 
 const app = express()
 
 app.use(cors());
 app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.giumd.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 // const uri = "mongodb+srv://volunteer-network:volunteerNetwork7373@cluster0.j05gt.mongodb.net/volunteer-network?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.get('/', (req, res) => {
+  res.send('Hello!')
+})
+
 client.connect(err => {
   const addEvents = client.db("volunteer-network").collection("eventRegister");
 
@@ -37,8 +44,6 @@ app.get('/registerUserEvents', (req, res) => {
   })
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello!')
-})
+
 
 app.listen(process.env.PORT || port);
